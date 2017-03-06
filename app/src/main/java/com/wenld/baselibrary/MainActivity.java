afefa1199838e3c43025cec64d922902e7b66609
LiDongWen <wenld2014@163.com>
2017-03-06 23:52:17 +0800

    万能dialog

--- app/src/main/java/com/wenld/baselibrary/MainActivity.java
+++ app/src/main/java/com/wenld/baselibrary/MainActivity.java
@@ -3,12 +3,19 @@ package com.wenld.baselibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.wenld.baselib.dialog.AlertDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new DefaultNavigationBar.Builder(this,null).create();
+//        new DefaultNavigationBar.Builder(this,null).create();
+
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setContentView(R.layout.detail_comment_dialog).fullWidth()
+                .fromBottom(false)
+                .show();
     }
 }
---
+++ baselib/src/main/java/com/wenld/baselib/dialog/DialogViewHelper.java
@@ -0,0 +1,94 @@
+package com.wenld.baselib.dialog;
+
+import android.content.Context;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Email 240336124@qq.com
+ * Created by Darren on 2017/2/21.
+ * Version 1.0
+ * Description: Dialog View的辅助处理类
+ */
+class DialogViewHelper {
+
+    private View mContentView = null;
+    // 防止霸气侧漏
+    private SparseArray<WeakReference<View>> mViews;
+
+    public DialogViewHelper(Context context, int layoutResId) {
+        this();
+        mContentView = LayoutInflater.from(context).inflate(layoutResId, null);
+    }
+
+
+    public DialogViewHelper() {
+        mViews = new SparseArray<>();
+    }
+
+    /**
+     * 设置布局View
+     *
+     * @param contentView
+     */
+    public void setContentView(View contentView) {
+        this.mContentView = contentView;
+    }
+
+    /**
+     * 设置文本
+     *
+     * @param viewId
+     * @param text
+     */
+    public void setText(int viewId, CharSequence text) {
+        // 每次都 findViewById   减少findViewById的次数
+        TextView tv = getView(viewId);
+        if (tv != null) {
+            tv.setText(text);
+        }
+    }
+
+    public <T extends View> T getView(int viewId) {
+        WeakReference<View> viewReference = mViews.get(viewId);
+        // 侧漏的问题  到时候到这个系统优化的时候再去介绍
+        View view = null;
+        if (viewReference != null) {
+            view = viewReference.get();
+        }
+
+        if (view == null) {
+            view = mContentView.findViewById(viewId);
+            if (view != null) {
+                mViews.put(viewId, new WeakReference<>(view));
+            }
+        }
+        return (T) view;
+    }
+
+    /**
+     * 设置点击事件
+     *
+     * @param viewId
+     * @param listener
+     */
+    public void setOnclickListener(int viewId, View.OnClickListener listener) {
+        View view = getView(viewId);
+        if (view != null) {
+            view.setOnClickListener(listener);
+        }
+    }
+
+    /**
+     * 获取ContentView
+     *
+     * @return
+     */
+    public View getContentView() {
+        return mContentView;
+    }
+}
